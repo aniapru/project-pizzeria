dea422e049db9b51330d190c3dac3ec8e9eb8e1a
Anna Prusińska <anna.prusinska12@gmail.com>
2023-02-25 02:39:17 +0100

    add server.mjs

--- src/js/settings.js
+++ src/js/settings.js
@@ -107,7 +107,10 @@ export const settings = {
     defaultMax: 10,
   },
   db: {
-    url: '//localhost:3131',
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     booking: 'bookings',
